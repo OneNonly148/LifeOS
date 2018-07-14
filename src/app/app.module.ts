e86a118d4af441c4887051087f34204a2164b08a
Wan Akashah <wanakashah14833@gmail.com>
2018-07-14 18:05:36 +0800

    Init bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
+import { RouterModule, Routes } from '@angular/router';
+// import {FlexLayoutModule} from "@angular/flex-layout";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material'; //import angular material components
+import {MatButtonModule, MatCheckboxModule, MatCardModule, MatToolbarModule,
+  MatInputModule, MatFormFieldModule, MatSelectModule} from '@angular/material'; //import angular material components
 
+const routes: Routes = [
+  { path: 'main', component: AppComponent},
+  { path: 'dashboard', component: DashboardComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    // FlexLayoutModule,
+    RouterModule.forRoot(routes),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireStorageModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule // declare imports from angular material
+    // declare imports from angular material
+    MatCardModule, MatToolbarModule, MatInputModule,
+    MatButtonModule, MatCheckboxModule, MatFormFieldModule,
+    MatSelectModule
+  ],
+  exports: [
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
